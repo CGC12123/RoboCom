6e08b29f0b25f80e3c6c46c50359b3c81def3bcf
CGC12123 <caiguoci@126.com>
2023-07-11 11:07:17 +0800

    加入左姿态切换

--- beetle_ai/scripts/left.py
+++ beetle_ai/scripts/left.py
@@ -5,10 +5,29 @@ import os
 import basic
 from GrabParams import grabParams
 
-# 运行至初始位置
+if os.path.exists("/home/robuster/beetle_ai/scripts/direction.txt"):
+    f = open("/home/robuster/beetle_ai/scripts/direction.txt", "r")
+    direction = int(f.read())
+    f.close()
+else:
+    f = open("/home/robuster/beetle_ai/scripts/direction.txt", "w")
+    f.write('0')
+    direction = 0
+    f.close()
+
 mc = MyCobot(grabParams.usb_dev, grabParams.baudrate)
-mc.set_color(0, 255, 255)  #运行，亮蓝灯
-mc.send_coords(grabParams.coords_left_high, 70, 0)
+mc.set_color(0,255,255)#运行，亮蓝灯
+
+if direction:  
+    mc.send_coords(grabParams.coords_left_high, 70, 0)
+    f = open("/home/robuster/beetle_ai/scripts/direction.txt", "w")
+    f.write('0')
+    f.close()
+else:
+    mc.send_coords(grabParams.coords_left_low, 70, 0)
+    f = open("/home/robuster/beetle_ai/scripts/direction.txt", "w")
+    f.write('1')
+    f.close()
 
 basic.grap(False)
-mc.set_color(0, 255, 0)  #调节结束，亮绿灯
+mc.set_color(0,255,0)#调节结束，亮绿灯
